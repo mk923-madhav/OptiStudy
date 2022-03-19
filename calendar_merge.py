740894357eaf1d3efbcce5cb5d7eac213f6508f2
madhavkannan <madhavkannan93@gmail.com>
2022-03-19 11:52:43 -0400

    added 1 experiment

--- calendar_merge.py
+++ calendar_merge.py
@@ -17,7 +17,7 @@ end_date=datetime.combine(end_date,datetime.max.time())
 #Initializing master calendar
 cal = Calendar()
 cwd = os.getcwd()
-location = cwd + "/Experiments/"
+location = cwd + "Experiments//"
 
 #Canvas file
 target_url = ""
