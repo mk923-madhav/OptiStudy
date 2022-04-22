7890fd70eb2ae77f94cbf78e6ed8dbfda16a7a6c
madhavkannan <madhavkannan93@gmail.com>
2022-04-22 04:23:09 -0400

    completing calendar view

--- calendar_merge.py
+++ calendar_merge.py
@@ -11,15 +11,14 @@ import os
 def create_calendar(location):
      #Set date markers
     start_date=str(request.form['startDate'])
-    start_date=datetime.strptime(start_date,"%d/%m/%Y").date()
+    start_date=datetime.strptime(start_date,"%Y-%m-%d").date()
     start_date=datetime.combine(start_date,datetime.min.time()) 
     end_date=str(request.form['endDate']) 
-    end_date=datetime.strptime(end_date,"%d/%m/%Y").date() 
+    end_date=datetime.strptime(end_date,"%Y-%m-%d").date() 
     end_date=datetime.combine(end_date,datetime.max.time())
 
     #Initializing master calendar
     cal = Calendar()
-    location = location + "/"
     display_events = []
 
     #Canvas file
@@ -27,41 +26,56 @@ def create_calendar(location):
     print (target_url)
     canvas_file = urllib.request.urlopen(target_url)
     calendar = Calendar.from_ical(canvas_file.read())
-    with open(location+"schedule.csv", 'w', newline='') as csvfile:
+    with open(location+"/schedule.csv", 'w', newline='') as csvfile:
         schedulewriter = csv.writer(csvfile, delimiter=',', quotechar=',', quoting=csv.QUOTE_MINIMAL)
         schedulewriter.writerow(["Summary", "Type", "Start", "End", "Duration"])
         events = recurring_ical_events.of(calendar).between(start_date, end_date)
         for event in events:
             if ('assignment' in event['uid']):
                 cal.add_component(event)
-                display_event = {
-                    'todo' : event["SUMMARY"],
-                    'start' :(event["DTSTART"].dt-timedelta (hours=4)).replace(tzinfo=None),
-                    'end' : (event["DTEND"].dt-timedelta (hours=4)).replace(tzinfo=None)
-                }
-                display_events.append (display_event)
                 schedulewriter.writerow([event["SUMMARY"].replace (",",""), "To-do", (event["DTSTART"].dt-timedelta (hours=4)).replace(tzinfo=None), (event["DTEND"].dt-timedelta (hours=4)).replace(tzinfo=None)])
     canvas_file.close()
 
     #Scheduler file
-    scheduler_file = open (location+"scheduler.ics",'rb')
+    scheduler_file = open (location+"/scheduler.ics",'rb')
     calendar = Calendar.from_ical(scheduler_file.read())
-    with open(location+"schedule.csv", 'a', newline='') as csvfile:
+    with open(location+"/schedule.csv", 'a', newline='') as csvfile:
         schedulewriter = csv.writer(csvfile, delimiter=',', quotechar=',', quoting=csv.QUOTE_MINIMAL)
         events = recurring_ical_events.of(calendar).between(start_date, end_date)
         for event in events:
             cal.add_component(event)
-            display_event = {
-                'todo' : event["SUMMARY"],
-                'start' :event["DTSTART"].dt,
-                'end' : event["DTEND"].dt
-            }
-            display_events.append (display_event)
             schedulewriter.writerow([event["SUMMARY"].replace (",",""), "Class", event["DTSTART"].dt, event["DTEND"].dt, event["DTEND"].dt - event["DTSTART"].dt])
     scheduler_file.close()
 
-    f = open(location+'optistudy_calendar.ics', 'wb')
+    f = open(location+'/optistudy_calendar.ics', 'wb')
     f.write(cal.to_ical())
     f.close()
 
+    display_events = populate_events(location)
     return display_events
+
+def populate_events (location):
+    events = []
+    with open(location+"/schedule.csv", newline='') as csvfile:
+        schedule_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in schedule_reader:
+            if (row[1]) == "To-do" :
+                event = {
+                    'todo' : row[0],
+                    'start' : row[2],
+                    'end' : row[3],
+                    'color' : '#F8E9E8',
+                    'editable' : 'false'
+                }
+                events.append (event)
+            elif (row[1]) == "Class" :
+                event = {
+                    'todo' : row[0],
+                    'start' : row[2],
+                    'end' : row[3],
+                    'color' : '#E9F6FC',
+                    'editable' : 'false'
+                }
+                events.append (event)
+            
+    return events
--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,5 @@
 from asyncio import events
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, current_app
 from calendar_merge import *
 from datetime import datetime
 
@@ -9,7 +9,11 @@ app = Flask(__name__, template_folder="templates")
 def main():
     return render_template('index.html')
 
-@app.route("/calendar", methods=['GET', 'POST'])
+@app.route("/preferences", methods=['GET', 'POST'])
+def check_preferences():
+    return render_template('preferences.html')
+
+@app.route("/basic_calendar", methods=['GET', 'POST'])
 def show_calendar():
     cwd = os.getcwd()
     location = os.path.join(cwd, "Experiments/", datetime.now().strftime("%m.%d.%Y,%H.%M.%S"))
@@ -18,16 +22,16 @@ def show_calendar():
     file = request.files['file']
     file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'scheduler.ics'))
     events = create_calendar(location)
-    return render_template ("calendar1.html", events=events)
+    return render_template ("basic_calendar.html", events=events)
+
+@app.route("/calendar", methods=['GET', 'POST'])
+def show_advanced_calendar():
+    events = populate_events(current_app.config['UPLOAD_FOLDER'])
+    return render_template ("advanced_calendar.html", events=events)
 
 @app.route("/download", methods=['GET'])
 def download_file():
     return send_from_directory(app.config["UPLOAD_FOLDER"], 'optistudy_calendar.ics', )
-
-# @app.route("/calendar", methods=['GET', 'POST'])
-# def calendar():
-
-#     return render_template('calendar1.html', events=events)
 
 if __name__ == "__main__":
 	# setDefaultEncoding ()
