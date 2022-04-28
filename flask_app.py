936c3b25cd122c04b064ab6ed689c6084d005346
madhavkannan <madhavkannan93@gmail.com>
2022-04-28 11:36:51 -0400

    cosmetic changes and session stickiness

--- flask_app.py
+++ flask_app.py
@@ -2,8 +2,11 @@ from asyncio import events
 from flask import Flask, render_template, send_from_directory, current_app
 from calendar_merge import *
 from datetime import datetime
+import os
 
 app = Flask(__name__, template_folder="templates")
+app.config['SECRET_KEY'] = os.urandom(24)
+app.config['SESSION_COOKIE_NAME'] = "my_session"
 
 @app.route("/", methods=['GET'])
 def main():
@@ -18,15 +21,15 @@ def show_calendar():
     cwd = os.getcwd()
     location = os.path.join(cwd, "Experiments/", datetime.now().strftime("%m.%d.%Y,%H.%M.%S"))
     os.mkdir (location)
-    app.config['UPLOAD_FOLDER'] = location
+    session['folderLocation'] = location
     file = request.files['file']
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'scheduler.ics'))
+    file.save(os.path.join(session['folderLocation'], 'scheduler.ics'))
     events = create_calendar(location)
     return render_template ("basic_calendar.html", events=events)
 
 @app.route("/calendar", methods=['GET', 'POST'])
 def show_advanced_calendar():
-    events = populate_events(current_app.config['UPLOAD_FOLDER'])
+    events = populate_events(session['folderLocation'])
     return render_template ("advanced_calendar.html", events=events)
 
 @app.route("/download", methods=['GET'])
